78ab6bd665c27f6bcfd39c56ac5c250943105df1
GucioWons <wgutek1@wp.pl>
2025-05-23 16:30:31 +0200

    Initial commit

---
+++ src/main/java/com/guciowons/yummify/YummifyApplication.java
@@ -0,0 +1,13 @@
+package com.guciowons.yummify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guciowons/yummify/YummifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guciowons.yummify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
