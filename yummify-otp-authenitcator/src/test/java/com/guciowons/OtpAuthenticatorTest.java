e365aefc3a9adf0ddda35931de2e6fb9484d3b6f
GucioWons <wgutek1@wp.pl>
2025-05-31 20:50:20 +0200

    Authentication-tests-scenarios

---
+++ yummify-otp-authenitcator/src/test/java/com/guciowons/OtpAuthenticatorTest.java
@@ -0,0 +1,85 @@
+package com.guciowons;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OtpAuthenticatorTest {
+
+    @Test
+    @DisplayName("Should authenticate")
+    void shouldAuthenticate() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when username param is null")
+    void shouldNotAuthenticateWhenUsernameParamIsNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when username param is blank")
+    void shouldNotAuthenticateWhenUsernameParamIsBlank() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp param is null")
+    void shouldNotAuthenticateWhenOtpParamIsNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp param is blank")
+    void shouldNotAuthenticateWhenOtpParamIsBlank() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when user not found")
+    void shouldNotAuthenticateWhenUserNotFound() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp expiration date attribute is null")
+    void shouldNotAuthenticateWhenOtpExpirationDateAttributeIsNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp expiration date attribute is blank")
+    void shouldNotAuthenticateWhenOtpExpirationDateAttributeIsBlank() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp attribute is null")
+    void shouldNotAuthenticateWhenOtpAttributeIsNull() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp attribute is blank")
+    void shouldNotAuthenticateWhenOtpAttributeIsBlank() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp expired")
+    void shouldNotAuthenticateWhenOtpExpired() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp expiration date is not valid")
+    void shouldNotAuthenticateWhenOtpExpirationDateIsNotValid() {
+
+    }
+
+    @Test
+    @DisplayName("Should not authenticate when otp is not valid")
+    void shouldNotAuthenticateWhenOtpIsNotValid() {
+
+    }
+}
