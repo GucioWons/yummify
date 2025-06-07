62fa251e3eb48e37452861f93adda8c2fa5f927d
GucioWons <wgutek1@wp.pl>
2025-06-07 22:48:39 +0200

    Created-single-api-error-exception-and-handler

--- yummify-server/src/main/java/com/guciowons/yummify/common/exception/dto/ApiErrorResponseDTO.java
+++ yummify-server/src/main/java/com/guciowons/yummify/common/exception/dto/ApiErrorResponseDTO.java
@@ -15,9 +15,15 @@ public class ApiErrorResponseDTO {
     private final LocalDateTime errorOccurredAt = LocalDateTime.now();
     private final List<ApiErrorDTO> apiErrorList;
 
-    public ApiErrorResponseDTO(List<ApiErrorDTO.Builder> apiErrorBuilderList, String path) {
-        this.apiErrorList = apiErrorBuilderList.stream().map(ApiErrorDTO.Builder::build).toList();
+    public ApiErrorResponseDTO(List<ApiErrorDTO> apiErrorList, String path) {
+        this.apiErrorList = apiErrorList;
         this.path = path;
         this.httpStatus = apiErrorList.getFirst().getHttpStatus();
+    }
+
+    public ApiErrorResponseDTO(ApiErrorDTO apiError, String path) {
+        this.apiErrorList = List.of(apiError);
+        this.path = path;
+        this.httpStatus = apiError.getHttpStatus();
     }
 }
--- yummify-server/src/main/java/com/guciowons/yummify/common/exception/handler/GlobalExceptionHandler.java
+++ yummify-server/src/main/java/com/guciowons/yummify/common/exception/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.guciowons.yummify.common.exception.handler;
 
+import com.guciowons.yummify.common.exception.SingleApiErrorException;
 import com.guciowons.yummify.common.exception.dto.ApiErrorDTO;
 import com.guciowons.yummify.common.exception.dto.ApiErrorResponseDTO;
 import com.guciowons.yummify.common.exception.enumerated.ErrorMessage;
@@ -11,20 +12,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.List;
-
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { Exception.class })
     protected ResponseEntity<Object> handleUnexpectedException(Exception e, WebRequest request) {
-        ApiErrorResponseDTO response = buildResponseDTO(request);
-        return handleExceptionInternal(e, buildResponseDTO(request), new HttpHeaders(), response.getHttpStatus(), request);
+        ApiErrorResponseDTO response = buildUnexpectedApiErrorResponseDTO(request);
+        return handleExceptionInternal(e, response, new HttpHeaders(), response.getHttpStatus(), request);
     }
 
-    private ApiErrorResponseDTO buildResponseDTO(WebRequest request) {
-        return new ApiErrorResponseDTO(
-                List.of(ApiErrorDTO.builder(ErrorMessage.UNEXPECTED_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR)),
-                request.getContextPath()
-        );
+    @ExceptionHandler(value = { SingleApiErrorException.class })
+    protected ResponseEntity<Object> handleUnexpectedException(SingleApiErrorException e, WebRequest request) {
+        ApiErrorResponseDTO response = buildSingleApiErrorResponseDTO(e.getApiError(), request);
+        return handleExceptionInternal(e, response, new HttpHeaders(), response.getHttpStatus(), request);
+    }
+
+    private ApiErrorResponseDTO buildUnexpectedApiErrorResponseDTO(WebRequest request) {
+        ApiErrorDTO apiError = ApiErrorDTO.builder(ErrorMessage.UNEXPECTED_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR).build();
+        return buildSingleApiErrorResponseDTO(apiError, request);
+    }
+
+    private ApiErrorResponseDTO buildSingleApiErrorResponseDTO(ApiErrorDTO apiError, WebRequest request) {
+        return new ApiErrorResponseDTO(apiError, request.getContextPath());
     }
 }
