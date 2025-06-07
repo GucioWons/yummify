4ea72e0f73e45380976ef3a7279fddf771656027
GucioWons <wgutek1@wp.pl>
2025-06-07 23:27:36 +0200

    Used-new-exceptions-in-keycloak-module

--- yummify-server/src/main/java/com/guciowons/yummify/common/exception/enumerated/ErrorMessage.java
+++ yummify-server/src/main/java/com/guciowons/yummify/common/exception/enumerated/ErrorMessage.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 public enum ErrorMessage {
     RESTAURANT_NOT_FOUND_BY_ID("Could not find restaurant with ID {{id}}"),
 
+    KEYCLOAK_ACCOUNT_EXISTS_BY_EMAIL("Account with this email already exists"),
+    KEYCLOAK_ACCOUNT_EXISTS_BY_USERNAME("Account with this username already exists"),
+
     UNEXPECTED_SERVER_ERROR("Unexpected server error");
 
     private final String message;
