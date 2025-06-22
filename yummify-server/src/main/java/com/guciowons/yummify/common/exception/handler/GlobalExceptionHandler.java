94e260eaa65a2dd39b162b6eb91d1e4fc1e5816a
GucioWons <wgutek1@wp.pl>
2025-06-22 14:53:00 +0200

    Handling-validation-exception

--- yummify-server/src/main/java/com/guciowons/yummify/common/exception/handler/GlobalExceptionHandler.java
+++ yummify-server/src/main/java/com/guciowons/yummify/common/exception/handler/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,9 +19,14 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Map<String, ErrorMessage> errorMessageMap = Map.of(
+            "Null", ErrorMessage.VALIDATION_NULL,
+            "NotNull", ErrorMessage.VALIDATION_NOT_NULL
+    );
 
     @ExceptionHandler(value = { Exception.class })
     protected ResponseEntity<Object> handleUnexpectedException(Exception e, WebRequest request) {
@@ -43,15 +49,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, @NonNull HttpHeaders headers, @NonNull HttpStatusCode statusCode, @NonNull WebRequest request) {
         List<ApiErrorDTO> apiErrors = e.getBindingResult().getFieldErrors().stream()
-                .map(fieldError -> {
-                    ErrorMessage message = ErrorMessage.VALIDATION_NULL;
-
-                    return ApiErrorDTO.builder(message, HttpStatus.BAD_REQUEST)
-                            .errorLocationType(ErrorLocationType.BODY)
-                            .location(fieldError.getField())
-                            .build();
-                })
+                .map(this::getFieldApiError)
                 .toList();
+
         ApiErrorResponseDTO response = buildMultipleApiErrorResponseDTO(apiErrors, request);
         return handleExceptionInternal(e, response, new HttpHeaders(), response.getHttpStatus(), request);
     }
@@ -70,5 +70,24 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private ApiErrorResponseDTO buildMultipleApiErrorResponseDTO(List<ApiErrorDTO> apiErrors, WebRequest request) {
         return new ApiErrorResponseDTO(apiErrors, request.getDescription(false));
+    }
+
+    private ApiErrorDTO getFieldApiError(FieldError apiError) {
+        ErrorMessage errorMessage = errorMessageMap.getOrDefault(
+                getValidationName(apiError.getCodes()),
+                ErrorMessage.UNEXPECTED_SERVER_ERROR
+        );
+
+        return ApiErrorDTO.builder(errorMessage, HttpStatus.UNPROCESSABLE_ENTITY)
+                .errorLocationType(ErrorLocationType.BODY)
+                .location(apiError.getField().replace(".", "/"))
+                .build();
+    }
+
+    private String getValidationName(String[] codes) {
+        if (codes == null || codes.length < 4) {
+            throw new IllegalArgumentException();
+        }
+        return codes[3];
     }
 }
