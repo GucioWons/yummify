492622441b92ee4eb48ae6f8ac3d1b599fb46225
GucioWons <wgutek1@wp.pl>
2025-11-22 00:47:17 +0100

    internationalization

--- yummify-manage-frontend/src/src/auth/page/LoginPage.tsx
+++ yummify-manage-frontend/src/src/auth/page/LoginPage.tsx
@@ -1,12 +1,16 @@
 import {useCallback, useContext} from "react";
 import keycloak from "../config/keycloak.ts";
 import {AuthContext} from "../context/AuthContext.tsx";
+import {useTranslation} from "react-i18next";
+import i18n from "i18next";
 
 function LoginPage() {
     const { saveUserAndToken } = useContext(AuthContext);
 
+    const { t } = useTranslation();
+
     const handleLogin = useCallback(() => {
-        keycloak.login().then(() => {
+        keycloak.login({locale: i18n.language}).then(() => {
             if (keycloak.token && keycloak.tokenParsed) {
                 saveUserAndToken(keycloak.tokenParsed, keycloak.token)
             }
@@ -15,7 +19,7 @@ function LoginPage() {
 
 
     return (
-        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogin}>{t("login")}</button>
     )
 }
 
