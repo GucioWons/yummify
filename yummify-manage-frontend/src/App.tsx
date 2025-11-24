98cdc5195f200c958f42536b356198842381998b
Gustaw Wąsowski <96374672+GucioWons@users.noreply.github.com>
2025-11-24 18:45:11 +0100

    internationalization (#31)

--- yummify-manage-frontend/src/App.tsx
+++ yummify-manage-frontend/src/App.tsx
@@ -2,17 +2,21 @@ import './App.css'
 import LoginPage from "./src/auth/page/LoginPage.tsx";
 import {AuthContext} from "./src/auth/context/AuthContext.tsx";
 import {useContext} from "react";
+import "./src/common/i18n/i18n";
+import {useTranslation} from "react-i18next";
 
 function App() {
   const { user, logout } = useContext(AuthContext);
+
+  const { t } = useTranslation();
 
   if (!user) {
     return <LoginPage />;
   }
 
   return <>
-      <div>Witaj!</div>;
-      <button onClick={logout}>Logout</button>
+      <div>{t("welcome", { name: user.username })}</div>;
+      <button onClick={logout}>{t("logout")}</button>
     </>
 }
 
