9e82392aec7d90d3adb23cecc5cc8d47be3ce8ab
GucioWons <wgutek1@wp.pl>
2025-12-14 18:49:04 +0100

    inputs-styling

--- yummify-manage-frontend/src/src/common/input/Input.tsx
+++ yummify-manage-frontend/src/src/common/input/Input.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Input.css"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
@@ -10,8 +11,8 @@ function Input(props: InputProps) {
     const {label, labelPosition = "top", placeholder, id, ...rest} = props;
 
     return (
-        <label className={`form-field ${labelPosition}`} htmlFor={id}>
-            {label && <span className="form-label">{label}</span>}
+        <label className={`input label-${labelPosition}`} htmlFor={id}>
+            {label && <span className="input-label">{label}</span>}
             <input placeholder={placeholder} id={id} {...rest} />
         </label>
     );
