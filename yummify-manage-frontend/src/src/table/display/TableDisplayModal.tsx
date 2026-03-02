efef80f4ed80e1286c0e72019baa481b2e42fd0b
GucioWons <wgutek1@wp.pl>
2026-03-02 19:55:12 +0100

    Corrected-display-modal-titles

--- yummify-manage-frontend/src/src/table/display/TableDisplayModal.tsx
+++ yummify-manage-frontend/src/src/table/display/TableDisplayModal.tsx
@@ -1,6 +1,6 @@
 import {Dtos} from "../../common/dtos.ts";
 import Modal from "../../common/modal/Modal.tsx";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import TableDisplay from "./TableDisplay.tsx";
 import TableDto = Dtos.TableDto;
 import TableUpdateForm from "./TableUpdateForm.tsx";
@@ -15,8 +15,12 @@ function TableDisplayModal(props: TableDisplayModalProps) {
 
     const [isInEditState, setIsInEditState] = useState(false);
 
+    const getTitle = useCallback(() => {
+        return isInEditState ? "Edit Table" : "Table Details";
+    }, [isInEditState]);
+    
     return (
-        <Modal title="Table Details" onClose={onClose}>
+        <Modal title={getTitle()} onClose={onClose}>
             {isInEditState
                 ? <TableUpdateForm table={table} onCancel={() => setIsInEditState(false)}/>
                 : <TableDisplay table={table} onEditClick={() => setIsInEditState(true)} onCloseClick={onClose}/>
